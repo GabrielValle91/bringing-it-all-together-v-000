7b6e5da909a956a65dbc39de8221a8bdfa3cbce8
Gabriel Valle <gvalle91@gmail.com>
2018-06-28 21:04:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,6 +9,18 @@ class Dog
     @@all << self
   end
 
+  def self.new_from_db(row)
+
+  end
+
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, id).first
+  end
+
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS dogs (
