6a56e2de2677146d762cd18428cb32d2f83637c0
Gabriel Valle <gvalle91@gmail.com>
2018-06-28 21:01:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,10 @@ class Dog
   attr_accessor :id, :name, :breed
 
   def initialize(name:, breed:, id: nil)
-
+    @name = name
+    @breed = breed
+    @id = id
+    @@all << self
   end
 
   def self.create_table
